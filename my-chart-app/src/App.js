ce892420f9a60ca277c78844145ca9454041bc59
zhengri <zhengri@gmail.com>
2024-02-05 21:07:41 +0800

    create blank page

---
+++ my-chart-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+     return (
+       <div className="App">
+         <header className="App-header">
+           {/* Add your contents */}
+         </header>
+       </div>
+     );
+   }
+export default App;
