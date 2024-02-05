c3b5d96dccfd386663754b938e77eabe74c755f8
Ulysses <ulyssesfm@gmail.com>
2024-02-04 21:28:54 -0800

    Add dummy data

---
+++ data/wa_employment_dec2023.js
@@ -0,0 +1,54 @@
+const data01 = [
+  {
+    "industry": "Mining and Logging",
+    "value": 5800
+  },
+  {
+    "industry": "Construction",
+    "value": 232400
+  },
+  {
+    "industry": "Manufacturing",
+    "value": 279600
+  },
+  {
+    "industry": "Wholesale Trade",
+    "value": 140600
+  },
+  {
+    "industry": "Retail Trade",
+    "value": 335100
+  },
+  {
+    "industry": "Transportation, Warehousing and Utilities",
+    "value": 142100
+  },
+  {
+    "industry": "Information",
+    "value": 163000
+  },
+  {
+    "industry": "Financial Activities",
+    "value": 170300
+  },
+  {
+    "industry": "Professional and Business Services",
+    "value": 560100
+  },
+  {
+    "industry": "Education and Health Services",
+    "value": 558200
+  },
+  {
+    "industry": "Leisure and Hospitality",
+    "value": 354700
+  },
+  {
+    "industry": "Other Services",
+    "value": 125400
+  },
+  {
+    "industry": "Government",
+    "value": 586300
+  }
+];
